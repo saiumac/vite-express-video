ff8f2147cb0bfc91201b975c58c1b7e367fa97e7
saiumac <gaddebunty24@gmail.com>
2024-10-08 15:45:39 +0530

    Update install_dependencies.sh

--- server/scripts/install_dependencies.sh
+++ server/scripts/install_dependencies.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd /var/www/vite-express-video/server
+
+# Install Node.js and npm if not already installed
+if ! command -v npm &> /dev/null
+then
+    curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+    sudo yum install -y nodejs
+fi
+
+# Install dependencies
 npm install
